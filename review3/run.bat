1602efec8e602deb7656599503f44192cb9ceee4
Leon Si <leonzalion@gmail.com>
2021-09-27 23:40:35 -0400

    fix

--- review3/run.bat
+++ review3/run.bat
@@ -1,29 +1,29 @@
 echo P1
 javac P1.java
 pause
-java Review1P1
+java Review3P1
 pause
 
 echo P6
 javac P6.java
 pause
-java Review1P6
+java Review3P6
 pause
 
 echo P7
 javac P7.java
 pause
-java Review1P7
+java Review3P7
 pause
 
 echo P9
 javac P9.java
 pause
-java Review1P9
+java Review3P9
 pause
 
 echo P10
 javac P10.java
 pause
-java Review1P10
+java Review3P10
 pause
--- review4/run.bat
+++ review4/run.bat
@@ -1,29 +1,29 @@
 echo P1
 javac P1.java
 pause
-java Review1P1
+java Review4P1
 pause
 
 echo P2
 javac P2.java
 pause
-java Review1P2
+java Review4P2
 pause
 
 echo P3
 javac P3.java
 pause
-java Review1P3
+java Review4P3
 pause
 
 echo P4
 javac P4.java
 pause
-java Review1P4
+java Review4P4
 pause
 
 echo P5
 javac P5.java
 pause
-java Review1P5
+java Review4P5
 pause
