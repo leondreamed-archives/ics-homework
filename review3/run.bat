7f13bbc7f2a6e68000f1e91ef1a11184e5f76aca
Leon Si <leonzalion@gmail.com>
2021-09-27 23:30:27 -0400

    run bat

---
+++ review3/run.bat
@@ -0,0 +1,24 @@
+javac P1.java
+pause
+java Review1P1
+pause
+
+javac P6.java
+pause
+java Review1P6
+pause
+
+javac P7.java
+pause
+java Review1P7
+pause
+
+javac P9.java
+pause
+java Review1P9
+pause
+
+javac P10.java
+pause
+java Review1P10
+pause
---
+++ review4/run.bat
@@ -0,0 +1,25 @@
+
+javac P1.java
+pause
+java Review1P1
+pause
+
+javac P2.java
+pause
+java Review1P2
+pause
+
+javac P3.java
+pause
+java Review1P3
+pause
+
+javac P4.java
+pause
+java Review1P4
+pause
+
+javac P5.java
+pause
+java Review1P5
+pause
