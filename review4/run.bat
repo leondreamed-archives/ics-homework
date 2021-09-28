7fc20e5c05f599d2f09afddc9f7886ebbe854834
Leon Si <leonzalion@gmail.com>
2021-09-27 23:40:01 -0400

    echo

--- review4/run.bat
+++ review4/run.bat
@@ -1,24 +1,28 @@
-
+echo P1
 javac P1.java
 pause
 java Review1P1
 pause
 
+echo P2
 javac P2.java
 pause
 java Review1P2
 pause
 
+echo P3
 javac P3.java
 pause
 java Review1P3
 pause
 
+echo P4
 javac P4.java
 pause
 java Review1P4
 pause
 
+echo P5
 javac P5.java
 pause
 java Review1P5
