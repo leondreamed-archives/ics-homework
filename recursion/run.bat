2e3138b38d45f97a284599e1aaff6e54de0ca0c6
Leon Si <contact@leonzalion.com>
2021-11-25 00:24:51 -0500

    add run.bat

---
+++ recursion/run.bat
@@ -0,0 +1,35 @@
+echo Power
+javac Power.java
+pause
+java Power
+pause
+
+echo Square
+javac Square.java
+pause
+java Square
+pause
+
+echo Prime
+javac Prime.java
+pause
+java Prime
+pause
+
+echo Sequence
+javac Sequence.java
+pause
+java Sequence
+pause
+
+echo Golf
+javac Golf.java
+pause
+java Golf
+pause
+
+echo BitPatterns
+javac BitPatterns.java
+pause
+java BitPatterns
+pause
