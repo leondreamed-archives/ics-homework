d0f65224c07c17cc09ac7c36f46f3a2462cc3d32
Leon Si <contact@leonzalion.com>
2021-11-25 00:19:09 -0500

    finish golf

--- recursion/Golf.java
+++ recursion/Golf.java
@@ -7,27 +7,31 @@ class GolfResult {
 
 class Golf {
 	public static int golf1(int holeLength, int[] clubLengths) {
-		int numShots = 0;
-		for (int i = 0; i < clubLengths.length; i += 1) {
-			int numShotsWithClub = holeLength / clubLengths[i];
-			numShots += numShotsWithClub;
-			holeLength -= numShotsWithClub * clubLengths[i];
+		if (holeLength <= 0) {
+			return 0;
 		}
-		return numShots;
+		return 1 + golf1(holeLength - clubLengths[0], clubLengths);
 	}
 
 	// Returns the least number of shots. Returns -1 when it is impossible
 	// to get to the hole
 	public static int golf2(int holeLength, int[] clubLengths) {
-		if (holeLength == 0) return 0;
+		if (holeLength == 0) {
+			return 0;
+		}
+
 		int minShots = Integer.MAX_VALUE;
 		for (int clubLength : clubLengths) {
 			if (clubLength <= holeLength) {
-			int numShots = 1 + golf2(holeLength - clubLength, clubLengths);
-			if (numShots != -1 && numShots < minShots) {
-				minShots = numShots;
+				int numShots = golf2(holeLength - clubLength, clubLengths);
+				if (numShots == -1) {
+					continue;
+				}
+				numShots += 1;
+				if (numShots < minShots) {
+					minShots = numShots;
+				}
 			}
-		}
 		}
 
 		if (minShots == Integer.MAX_VALUE) {
@@ -45,7 +49,6 @@ class Golf {
 		return numShots;
 	}
 
-
 	// Returns an array where each index represents the club and the value
 	// represents the number of times the club was used. If there was no solution,
 	// it returns an empty array.
@@ -53,14 +56,17 @@ class Golf {
 		int[] numShotsPerClub = new int[clubLengths.length];
 		int minClub = Integer.MAX_VALUE;
 
-		if (holeLength == 0) return numShotsPerClub;
+		if (holeLength == 0) {
+			return numShotsPerClub;
+		}
 
 		int minShots = Integer.MAX_VALUE;
 		for (int i = 0; i < clubLengths.length; i += 1) {
 			int clubLength = clubLengths[i];
 			if (clubLength <= holeLength) {
 				int[] curNumShotsPerClub = golf3(holeLength - clubLength, clubLengths);
-				if (curNumShotsPerClub.length == 0) continue;
+				if (curNumShotsPerClub.length == 0)
+					continue;
 
 				int numShots = getNumShots(curNumShotsPerClub);
 				if (numShots != -1 && numShots < minShots) {
@@ -91,15 +97,33 @@ class Golf {
 		for (int i = 0; i < numClubs; ++i) {
 			clubLengths[i] = sc.nextInt();
 		}
+
+		System.out.println("Version 1:");
+		System.out.println("It will take " + golf1(holeLength, clubLengths) + " shots to get to the hole.");
+		System.out.println();
+
+		System.out.println("Version 2:");
+		int numShots = golf2(holeLength, clubLengths);
+		if (numClubs == -1) {
+			System.out.println("The hole cannot be reached.");
+		} else {
+			System.out.println("The minimum number of shots it takes to get to the hole is " + numShots + ".");
+		}
+		System.out.println();
+
+		System.out.println("Version 3:");
 		int[] numShotsPerClub = golf3(holeLength, clubLengths);
 		if (numShotsPerClub.length == 0) {
 			System.out.println("The hole cannot be reached.");
-			return;
+		} else {
+			System.out
+					.println("The minimum number of shots it takes to get to the hole is " + getNumShots(numShotsPerClub) + ".");
+			System.out.println("Number of times each club was used:");
+			for (int i = 0; i < numClubs; ++i) {
+				System.out.println("Club " + (i + 1) + ": " + numShotsPerClub[i]);
+			}
 		}
 
-		System.out.println("Number of times each club was used:");
-		for (int i = 0; i < numClubs; ++i) {
-			System.out.println("Club " + (i + 1) + ": " + numShotsPerClub[i]);
-		}
+		sc.close();
 	}
 }
