f0941383ca9807472c905a5e859dbaa2829f98f8
Leon Si <contact@leonzalion.com>
2021-11-25 00:20:42 -0500

    update

--- recursion/Sequence.java
+++ recursion/Sequence.java
@@ -1,7 +1,9 @@
 class Sequence {
 	// Recursive definition of kth term: k_n = k_(n-1) * k_(n-2)
+	// Exit condition: k == 1 or k == 2
+	// k == 1 returns 4
+	// k == 2 returns 5
 	public static int sequence(int k) {
-		// Exit condition: k == 1 or k == 2
 		if (k == 1) return 4;
 		if (k == 2) return 5;
 		return sequence(k + 1) + sequence(k - 2);
